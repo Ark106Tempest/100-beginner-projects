71c99a6f531276835b6aa46cb403e97d1b300c3d
Ark Tempest <ark@ark.ark>
2026-08-09 13:21:22 +0530

    project #1 completed

---
+++ calculator-v1.py
@@ -0,0 +1,20 @@
+num1 = 2
+num2 = 3
+
+wtd = "+"
+
+if wtd == "+":
+	print(num1 + num2)
+elif wtd == "-":
+	print(num1 - num2)
+elif wtd == "*":
+	print(num1 * num2)
+elif wtd == "/":
+	print(num1 / num2)
+elif wtd == "**":
+	print(num1 ** num2)
+elif wtd == "//":
+	print(num1 // num2)
+else:
+	print("something went wrong")
+
