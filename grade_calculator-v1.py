7f4ec35ee9cfc7711ad0ca16e1213d357da92c40
Ark Tempest <asifak47op@gmail.com>
2026-08-27 16:30:38 +0530

    project #42 completed

---
+++ grade_calculator-v1.py
@@ -0,0 +1,9 @@
+num1 = float(input("sub1: "))
+num2 = float(input("sub2: "))
+num3 = float(input("sub3: "))
+num4 = float(input("sub4: "))
+num5 = float(input("sub5: "))
+marks = float(input("how much is total marks: "))
+total = num1 + num2 + num3 + num4 + num5
+print("total marks is", total)
+print("percentage is", (total / marks) * 100)
