f4c1066834d2a8531b0cfb9d2cc05acabcccbe20
Ark Tempest <asifak47op@gmail.com>
2026-08-28 17:53:24 +0530

    project #47 completed

---
+++ rock_paper_scissors-v2.py
@@ -0,0 +1,68 @@
+from random import choice
+from time import sleep
+score = {
+        "player": 0,
+        "bot": 0,
+        "ties": 0
+        }
+run = True
+while run:
+    print("1. play\n2. settings\n3. exit")
+    menu = int(input("choose from the options above: "))
+    if menu == 1:
+        mode = True
+        while mode:
+            print("1. normal mode\n2. flow mode\n3. kid mode\n4. selective rounds\n5. back") #option 2,3 and 4 are currently unavailable
+            option = int(input("select the game mode: "))
+            if option == 1:
+                play = True
+                while play:
+                    player = input("type your move secretly: ")
+                    bot = choice(["rock","paper","scissors"])
+                    print("ready")
+                    sleep(0.5)
+                    print("set")
+                    sleep(0.5)
+                    print("go")
+                    sleep(0.5)
+                    print("final result is...")
+                    sleep(1)
+                    print(f"bot's move is {bot}")
+                    if (player == "rock" and bot == "rock") or (player == "paper" and bot == "paper") or (player == "scissors" and bot == "scissors"):
+                        print("it's a tie")
+                        score["ties"] = score["ties"] + 1
+                        print(score)
+                    elif (player == "rock" and bot == "paper") or (player == "paper" and bot == "scissors") or (player == "scissors" and bot == "rock"):
+                        print("you have lost")
+                        score["bot"] = score["bot"] + 1
+                        print(score)
+                    elif (player == "rock" and bot == "scissors") or (player == "paper" and bot == "rock") or (player == "scissors" and bot == "paper"):
+                        print("you have won")
+                        score["player"] = score["player"] + 1
+                    print("would you like to play again \npress enter to play again or type 'back' to go back")
+                    decision = True
+                    while decision:
+                        again = input("what would you like to do?: ")
+                        if again == "":
+                            play = True
+                            decision = False
+                        elif again == "back":
+                            play = False
+                            decision = False
+                        else:
+                            print("something went wrong plz try again")
+            elif option == 2:
+                print("this option is currently unavailable")
+            elif option == 3:
+                print("this option is currently unavailable")
+            elif option == 4:
+                print("this option is currently unavailable")
+            elif option == 5:
+                mode = False
+            else:
+                print("something went wrong plz try again")
+    elif menu == 2:
+        print("this option is currently unavailable")
+    elif menu == 3:
+        print("bye")
+        run = False 
