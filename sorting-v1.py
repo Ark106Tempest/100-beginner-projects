4821b8e4e21221a9f29ec17e45349b2bdff75caa
Ark Tempest <asifak47op@gmail.com>
2026-09-18 18:57:07 +0530

    deleted somthing

--- sorting-v1.py
+++
@@ -1,8 +0,0 @@
-nums = [2,6,1,7,8,4,5,3,9]
-for x in range(0, len(nums), 1):
-    for y in range(x + 1, len(nums), 1):
-        if nums[x] > nums[y]:
-            a = nums[x]
-            nums[x] = nums[y]
-            nums[y] = a
-print(nums)
